de5aa9aaa7fa5bac24cd72d44de0f4aa51fddb02
basnt ebrahim <161939675+BasntEbrahim7@users.noreply.github.com>
2024-08-29 16:39:42 +0300

    task4.dart

--- task4.dart
+++ task4.dart
@@ -17,13 +17,13 @@ void main() {
       body: Container(
         color: Color(0xFFF4F5F7),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // Align the content to the start (left)
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 13),
             Align(
-              alignment: Alignment.centerLeft, // Align the text to the left
+              alignment: Alignment.centerLeft,
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0), // Add padding if necessary
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Text(
                   "Choose your area",
                   style: TextStyle(color: Color(0xFF333030), fontSize: 20),
@@ -34,57 +34,64 @@ void main() {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  width: 145,
-                  height: 157,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.shopping_cart_outlined,
-                          color: Colors.white,
-                          size: 50,
-                        ),
-                        SizedBox(height: 15),
-                        Text(
-                          "Buying",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
+                InkWell(
+                  onTap: () {print('select buying');
+                    // Action for "Buying"
+                  },
+                  child: Container(
+                    width: 145,
+                    height: 157,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.shopping_cart_outlined,
+                            color: Colors.white,
+                            size: 50,
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            "Buying",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xFF635FE9),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFF635FE9),
+                    ),
                   ),
                 ),
                 SizedBox(width: 2),
-                Container(
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
+                InkWell(
+                  onTap: () {print("select selling");
+                    // Action for "Selling"
+                  },
+                  child: Container(
+                    width: 145,
+                    height: 157,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
                             Icons.house_outlined,
                             color: Color(0xFF635FE9),
                             size: 50,
                           ),
-                        ),
-                        Text(
-                          "Selling",
-                          style: TextStyle(color: Colors.black45),
-                        )
-                      ],
+                          Text(
+                            "Selling",
+                            style: TextStyle(color: Colors.black45),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  width: 145,
-                  height: 157,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xFFFDFDFD),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFFFDFDFD),
+                    ),
                   ),
                 )
               ],
@@ -93,57 +100,64 @@ void main() {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  width: 145,
-                  height: 157,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.business_center_rounded,
-                          color: Color(0xFF635FE9),
-                          size: 50,
-                        ),
-                        SizedBox(height: 15),
-                        Text(
-                          "Trades",
-                          style: TextStyle(color: Colors.black45),
-                        )
-                      ],
+                InkWell(
+                  onTap: () {
+                    // Action for "Trades"
+                  },
+                  child: Container(
+                    width: 145,
+                    height: 157,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.business_center_rounded,
+                            color: Color(0xFF635FE9),
+                            size: 50,
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            "Trades",
+                            style: TextStyle(color: Colors.black45),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
                   ),
                 ),
                 SizedBox(width: 2),
-                Container(
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
+                InkWell(
+                  onTap: () {
+                    // Action for "Videos"
+                  },
+                  child: Container(
+                    width: 145,
+                    height: 157,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
                             Icons.play_circle_outline,
                             color: Color(0xFF635FE9),
                             size: 50,
                           ),
-                        ),
-                        Text(
-                          "Videos",
-                          style: TextStyle(color: Colors.black45),
-                        )
-                      ],
+                          Text(
+                            "Videos",
+                            style: TextStyle(color: Colors.black45),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  width: 145,
-                  height: 157,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xFFFDFDFD),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFFFDFDFD),
+                    ),
                   ),
                 )
               ],
@@ -152,77 +166,88 @@ void main() {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  width: 145,
-                  height: 157,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.percent,
-                          color: Color(0xFF635FE9),
-                          size: 50,
-                        ),
-                        SizedBox(height: 15),
-                        Text(
-                          "Deals",
-                          style: TextStyle(color: Colors.black45),
-                        )
-                      ],
+                InkWell(
+                  onTap: () {
+                    // Action for "Deals"
+                  },
+                  child: Container(
+                    width: 145,
+                    height: 157,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.percent,
+                            color: Color(0xFF635FE9),
+                            size: 50,
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            "Deals",
+                            style: TextStyle(color: Colors.black45),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
                   ),
                 ),
                 SizedBox(width: 2),
-                Container(
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: Icon(
+                InkWell(
+                  onTap: () {
+                    // Action for "Case study"
+                  },
+                  child: Container(
+                    width: 145,
+                    height: 157,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
                             Icons.menu_book_outlined,
                             color: Color(0xFF635FE9),
                             size: 50,
                           ),
-                        ),
-                        Text(
-                          "Case study",
-                          style: TextStyle(color: Colors.black45),
-                        )
-                      ],
+                          Text(
+                            "Case study",
+                            style: TextStyle(color: Colors.black45),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  width: 145,
-                  height: 157,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xFFFDFDFD),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFFFDFDFD),
+                    ),
                   ),
                 )
               ],
             ),
-            SizedBox(height: 80,),
+            SizedBox(height: 80),
             Container(
               color: Colors.white,
               height: 64,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Icon(Icons.home,color: Color(0xFF635FE9),),
-                  Icon(Icons.phone_android_sharp,color: Color(0xFFC3C7CA),),
-                  Icon(Icons.wechat_sharp,color: Color(0xFFC3C7CA),),
-                  Icon(Icons.search,color: Color(0xFFC3C7CA)),
-                  Icon(Icons.person_outline,color: Color(0xFFC3C7CA))
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.home, color: Color(0xFF635FE9))),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.phone_android_sharp, color: Color(0xFFC3C7CA))),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.wechat_sharp, color: Color(0xFFC3C7CA))),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.search, color: Color(0xFFC3C7CA))),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.person_outline, color: Color(0xFFC3C7CA))),
                 ],
               ),
-            )
-
+            ),
           ],
         ),
       ),
